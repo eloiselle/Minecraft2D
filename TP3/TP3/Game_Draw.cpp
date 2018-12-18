009ecd5fcc6f283ecf6dd481d94bfe780bdd7a3f
DeltaHit <emile.loiselle@hotmail.com>
2018-12-18 17:57:06 -0500

    Homing bullet targets the closest bat

--- TP3/TP3/Game_Draw.cpp
+++ TP3/TP3/Game_Draw.cpp
@@ -7,206 +7,206 @@
 // Affiche les graphiques a l'ecran
 void Game::drawWindow()
 {
-    _window.clear();
+	_window.clear();
 
-    updateWindowTitle();
-    drawGrid();
-    updateViews();
+	updateWindowTitle();
+	drawGrid();
+	updateViews();
 
-    drawPlayer();
-    drawFoes();
-    drawBullets();
-    drawShield();
-    drawMovableGui();
+	drawPlayer();
+	drawFoes();
+	drawBullets();
+	drawShield();
+	drawMovableGui();
 
-    drawThingsDirectlyOnTheScreen();
+	drawThingsDirectlyOnTheScreen();
 
-    _window.display();
+	_window.display();
 }
 
 // Met a jour les view
 void Game::updateViews()
 {
-    _view[FOLLOW].setCenter(
-        _player.getExactX(),
-        _player.getExactY());
+	_view[FOLLOW].setCenter(
+		_player.getExactX(),
+		_player.getExactY());
 
-    _view[FOLLOW_Y].setCenter(
-        _map.nbCol() * TILE_SIZE / 2,
-        _player.getExactY());
-    ////}
+	_view[FOLLOW_Y].setCenter(
+		_map.nbCol() * TILE_SIZE / 2,
+		_player.getExactY());
+	////}
 }
 
 // Met a jour le titre de la fenetre
 void Game::updateWindowTitle()
 {
-    // Ajoute de l'information dans le titre
-    //_extraTitle += "Mouse: " +
-    //    to_string((int)_mouseMagnet.getExactX()) + " " +
-    //    to_string((int)_mouseMagnet.getExactY());
-    //_extraTitle += "  Coord: " +
-    //    to_string(_mouseMagnet.getGridCol()) + " " +
-    //    to_string(_mouseMagnet.getGridLine());
+	// Ajoute de l'information dans le titre
+	//_extraTitle += "Mouse: " +
+	//    to_string((int)_mouseMagnet.getExactX()) + " " +
+	//    to_string((int)_mouseMagnet.getExactY());
+	//_extraTitle += "  Coord: " +
+	//    to_string(_mouseMagnet.getGridCol()) + " " +
+	//    to_string(_mouseMagnet.getGridLine());
 
-    _extraTitle += toolName[_currentTool];
+	_extraTitle += toolName[_currentTool];
 
-    _extraTitle += "        Lives : " + to_string((int)_player.getHp()) + "/" + to_string((int)_player.getHpMax());
-    _extraTitle += "        Game State : " + _appStateName[_appState];
-    _extraTitle += "        Score : " + to_string(_score);
+	_extraTitle += "        Lives : " + to_string((int)_player.getHp()) + "/" + to_string((int)_player.getHpMax());
+	_extraTitle += "        Game State : " + _appStateName[_appState];
+	_extraTitle += "        Score : " + to_string(_score);
 
-    if (_extraTitle != "")
-        _extraTitle = " : " + _extraTitle;
+	if (_extraTitle != "")
+		_extraTitle = " : " + _extraTitle;
 
-    // Change le titre
-    _window.setTitle(WINDOW_TITLE + _extraTitle);
+	// Change le titre
+	_window.setTitle(WINDOW_TITLE + _extraTitle);
 
-    // Reinitialise pour la prochain iteration
-    _extraTitle = "";
+	// Reinitialise pour la prochain iteration
+	_extraTitle = "";
 }
 
 // Affiche la grille du labyrinthe
 void Game::drawGrid()
 {
-    for (size_t l = 0; l < _map.nbLine(); l++)
-    {
-        for (size_t c = 0; c < _map.nbCol(); c++)
-        {
-            // Choix de la bonne tuile a afficher
-            BLOCK_TYPE t = _map.at(l, c).getType();
-            int v = _map.at(l, c).getVersion();
-            _tileSprite[t][v].setPosition(c*TILE_SIZE, l*TILE_SIZE);
-            _window.draw(_tileSprite[t][v]);
-        }
-    }
+	for (size_t l = 0; l < _map.nbLine(); l++)
+	{
+		for (size_t c = 0; c < _map.nbCol(); c++)
+		{
+			// Choix de la bonne tuile a afficher
+			BLOCK_TYPE t = _map.at(l, c).getType();
+			int v = _map.at(l, c).getVersion();
+			_tileSprite[t][v].setPosition(c*TILE_SIZE, l*TILE_SIZE);
+			_window.draw(_tileSprite[t][v]);
+		}
+	}
 }
 
 void Game::drawPlayer()
 {
-    _playerSprites[_animFrame][_animType].setPosition(_player.getExactX(), _player.getExactY());
-    _window.draw(_playerSprites[_animFrame][_animType]);
+	_playerSprites[_animFrame][_animType].setPosition(_player.getExactX(), _player.getExactY());
+	_window.draw(_playerSprites[_animFrame][_animType]);
 }
 
 void Game::drawShield()
 {
-    if (_currentTool == SPHERE_SHIELD)
-    {
-        for (size_t i = 0; i < NB_SHIELD; i++)
-        {
-            _bulletShape[FRIENDLY].setPosition(_shieldSphere[i].getExactX(), _shieldSphere[i].getExactY());
-            _window.draw(_bulletShape[FRIENDLY]);
-        }
-    }
+	if (_currentTool == SPHERE_SHIELD)
+	{
+		for (size_t i = 0; i < NB_SHIELD; i++)
+		{
+			_bulletShape[FRIENDLY].setPosition(_shieldSphere[i].getExactX(), _shieldSphere[i].getExactY());
+			_window.draw(_bulletShape[FRIENDLY]);
+		}
+	}
 }
 
 void Game::drawBullets()
 {
-    for (list<Bullet>::iterator b = _bullets.begin(); b != _bullets.end(); b++)
-    {
-        _bulletShape[b->isFriendly()].setPosition(b->getExactX(), b->getExactY());
-        _window.draw(_bulletShape[b->isFriendly()]);
-    }
+	for (list<Bullet>::iterator b = _bullets.begin(); b != _bullets.end(); b++)
+	{
+		_bulletShape[b->isFriendly()].setPosition(b->getExactX(), b->getExactY());
+		_window.draw(_bulletShape[b->isFriendly()]);
+	}
 }
 
 void Game::drawMovableGui()
 {
-    // Mouse Square
-    _mouseMagnet.setPositionExact(
-        _mouseCoord.getPosition().x,
-        _mouseCoord.getPosition().y);
+	// Mouse Square
+	_mouseMagnet.setPositionExact(
+		_mouseCoord.getPosition().x,
+		_mouseCoord.getPosition().y);
 
-    if (_currentTool == BUILD)
-    {
-        _mouseSquare.setPosition(
-            _mouseMagnet.getGridCol() * TILE_SIZE,
-            _mouseMagnet.getGridLine() * TILE_SIZE);
-        _window.draw(_mouseSquare);
-    }
+	if (_currentTool == BUILD)
+	{
+		_mouseSquare.setPosition(
+			_mouseMagnet.getGridCol() * TILE_SIZE,
+			_mouseMagnet.getGridLine() * TILE_SIZE);
+		_window.draw(_mouseSquare);
+	}
 
-    if (toolIsAShooter())
-    {
-        _aimingSight.setPosition(
-            _mouseCoord.getPosition().x,
-            _mouseCoord.getPosition().y);
-        _window.draw(_aimingSight);
-    }
+	if (toolIsAShooter())
+	{
+		_aimingSight.setPosition(
+			_mouseCoord.getPosition().x,
+			_mouseCoord.getPosition().y);
+		_window.draw(_aimingSight);
+	}
 }
 
 void Game::drawFoes()
 {
-    // Bats
-    for (Crawler& c : _bats)
-    {
-        _batSprite[_frameRun / 5 % 4][c.getDirection()].setPosition(
-            c.getExactX(),
-            c.getExactY());
-        _window.draw(_batSprite[_frameRun / 5 % 4][c.getDirection()]);
-    }
+	// Bats
+	for (Crawler& c : _bats)
+	{
+		_batSprite[_frameRun / 5 % 4][c.getDirection()].setPosition(
+			c.getExactX(),
+			c.getExactY());
+		_window.draw(_batSprite[_frameRun / 5 % 4][c.getDirection()]);
+	}
 
-    // Boss
-    if (_boss.isAlive())
-    {
-        _batSprite[_frameRun / 5 % 4][2].setScale(Vector2f(2, 2));
-        _batSprite[_frameRun / 5 % 4][2].setPosition(_boss.getExactX(), _boss.getExactY());
-        _window.draw(_batSprite[_frameRun / 5 % 4][2]);
-        _batSprite[_frameRun / 5 % 4][2].setScale(Vector2f(1, 1));
-    }
+	// Boss
+	if (_boss.isAlive())
+	{
+		_batSprite[_frameRun / 5 % 4][2].setScale(Vector2f(2, 2));
+		_batSprite[_frameRun / 5 % 4][2].setPosition(_boss.getExactX(), _boss.getExactY());
+		_window.draw(_batSprite[_frameRun / 5 % 4][2]);
+		_batSprite[_frameRun / 5 % 4][2].setScale(Vector2f(1, 1));
+	}
 }
 
 void Game::drawBossHealthBar()
 {
-    // Boss health bar
-    if (_boss.isAlive())
-    {
-        float hpRatio = (float)_boss.getHp() / _boss.getHpMax();
-        _bossHealthBar.setSize(Vector2f(
-            (MAP_WIDTH_PIXELS - 6) * hpRatio, // Le 6 est pour 2 fois la thickness de la bar
-            16));
-        _bossHealthBar.setPosition(3, 3);
-        _window.draw(_bossHealthBar);
-    }
+	// Boss health bar
+	if (_boss.isAlive())
+	{
+		float hpRatio = (float)_boss.getHp() / _boss.getHpMax();
+		_bossHealthBar.setSize(Vector2f(
+			(MAP_WIDTH_PIXELS - 6) * hpRatio, // Le 6 est pour 2 fois la thickness de la bar
+			16));
+		_bossHealthBar.setPosition(3, 3);
+		_window.draw(_bossHealthBar);
+	}
 }
 
 // Affiche les objets qui ne sont pas affecter par la view
 void Game::drawThingsDirectlyOnTheScreen()
 {
-    _window.setView(_view[NULL_VIEW]);
+	_window.setView(_view[NULL_VIEW]);
 
-    drawBossHealthBar();
+	drawBossHealthBar();
 
-    // Affichage du shader
-    if (_appState != RUNNING)
-    {
-        _window.draw(_shader);
-        _window.draw(_messageOnShader);
-    }
+	// Affichage du shader
+	if (_appState != RUNNING)
+	{
+		_window.draw(_shader);
+		_window.draw(_messageOnShader);
+	}
 
-    // Debug info
-    _debugInfo.setString(_debug);
-    _window.draw(_debugInfo);
-    _debug = "";
+	// Debug info
+	_debugInfo.setString(_debug);
+	_window.draw(_debugInfo);
+	_debug = "";
 
-    _window.draw(_mouseCoord);
+	_window.draw(_mouseCoord);
 
-    _window.setView(_view[_currentView]);
+	_window.setView(_view[_currentView]);
 }
 
 // Miroir d'un sprite sur l'axe vertical
 void Game::flipSpriteHorizontal(Sprite& s)
 {
-    s.setOrigin({ s.getLocalBounds().width, 0 });
-    s.setScale({ -1, 1 });
+	s.setOrigin({ s.getLocalBounds().width, 0 });
+	s.setScale({ -1, 1 });
 }
 
 // Affiche le contenu de map dans le _debug
 void Game::printMap()
 {
-    _debug += "\r\n";
-    for (size_t l = 0; l < 20; l++)
-    {
-        for (size_t c = 0; c < 20; c++)
-        {
-            _debug += _map.at(l, c).getType() + 48;
-        }
-        _debug += "\r\n";
-    }
+	_debug += "\r\n";
+	for (size_t l = 0; l < 20; l++)
+	{
+		for (size_t c = 0; c < 20; c++)
+		{
+			_debug += _map.at(l, c).getType() + 48;
+		}
+		_debug += "\r\n";
+	}
 }
--- TP3/TP3/Game_Events.cpp
+++ TP3/TP3/Game_Events.cpp
@@ -7,294 +7,305 @@
 // Quitte l'application
 void Game::quitApplication()
 {
-    _window.close();
-    exit(1);
+	_window.close();
+	exit(1);
 }
 
 // Gere les input qui doivent etre active une seule fois par repetition
 void Game::inputActivatedOnlyTheFirstTime()
 {
-    // Window Controls
-    while (_window.pollEvent(_event))
-    {
-        switch (_event.type)
-        {
-        case Event::Closed:
-            quitApplication();
-            break;
-        case Event::Resized:
-            handleResizeWindow();
-            break;
-        case Event::LostFocus:
-            _appState = PAUSED;
-            break;
-        case Event::KeyPressed:
-            if (_event.key.code == Keyboard::BackSpace)
-                init();
-            if (_event.key.code == Keyboard::P)
-                handlePausing();
-            if (_event.key.code == Keyboard::T && canTeleportAtMouse())
-                _player.setPositionExact(
-                    _mouseMagnet.getGridCol() * TILE_SIZE + (TILE_SIZE / 2),
-                    _mouseMagnet.getGridLine() * TILE_SIZE + TILE_SIZE - 1);
-        }
+	// Window Controls
+	while (_window.pollEvent(_event))
+	{
+		switch (_event.type)
+		{
+		case Event::Closed:
+			quitApplication();
+			break;
+		case Event::Resized:
+			handleResizeWindow();
+			break;
+		case Event::LostFocus:
+			_appState = PAUSED;
+			break;
+		case Event::KeyPressed:
+			if (_event.key.code == Keyboard::BackSpace)
+				init();
+			if (_event.key.code == Keyboard::P)
+				handlePausing();
+			if (_event.key.code == Keyboard::T && canTeleportAtMouse())
+				_player.setPositionExact(
+					_mouseMagnet.getGridCol() * TILE_SIZE + (TILE_SIZE / 2),
+					_mouseMagnet.getGridLine() * TILE_SIZE + TILE_SIZE - 1);
+		}
 
-        if (_appState == RUNNING)
-        {
-            if (_event.type == Event::MouseWheelMoved)
-                handleMouseWheelMoved();
-        }
-    }
+		if (_appState == RUNNING)
+		{
+			if (_event.type == Event::MouseWheelMoved)
+				handleMouseWheelMoved();
+		}
+	}
 }
 
 bool Game::canTeleportAtMouse()
 {
-    return isMouseInWindow()
-        && isInMap(_mouseMagnet)
-        && _map.at(_mouseMagnet.getGridLine(), _mouseMagnet.getGridCol()).getType() == EMPTY_BLOCK
-        && !areOnTheSameSquare(_mouseMagnet, _player);
+	return isMouseInWindow()
+		&& isInMap(_mouseMagnet)
+		&& _map.at(_mouseMagnet.getGridLine(), _mouseMagnet.getGridCol()).getType() == EMPTY_BLOCK
+		&& !areOnTheSameSquare(_mouseMagnet, _player);
 }
 
 // Gere les input qui doivent etre activer en continu
 void Game::inputActivatedInContinu()
 {
-    handleKeypress(); // TODO event allow to do it only once
+	handleKeypress(); // TODO event allow to do it only once
 
-    if (_appState == RUNNING)
-    {
-        if (_currentView == CAMERA && isMouseInWindow())
-            handleMouseOnWindowBorders();
+	if (_appState == RUNNING)
+	{
+		if (_currentView == CAMERA && isMouseInWindow())
+			handleMouseOnWindowBorders();
 
-        if (Mouse::isButtonPressed(Mouse::Left)
-            || Mouse::isButtonPressed(Mouse::Right))
-            handleMouseButtonPressed();
-    }
+		if (Mouse::isButtonPressed(Mouse::Left)
+			|| Mouse::isButtonPressed(Mouse::Right))
+			handleMouseButtonPressed();
+	}
 
-    updateViewlessMouseCoord();
+	updateViewlessMouseCoord();
 }
 
 // Evalue si l'etat de pause est change
 void Game::handlePausing()
 {
-    switch (_appState)
-    {
-    case Game::RUNNING:
-        _appState = PAUSED;
-        break;
-    case Game::PAUSED:
-        _appState = RUNNING;
-        break;
-    case Game::BOSS_KILLED:
-        init();
-        _appState = RUNNING;
-        break;
-    case Game::GAME_OVER:
-        init();
-        _appState = RUNNING;
-        break;
-    default:        break;
-    }
+	switch (_appState)
+	{
+	case Game::RUNNING:
+		_appState = PAUSED;
+		break;
+	case Game::PAUSED:
+		_appState = RUNNING;
+		break;
+	case Game::BOSS_KILLED:
+		init();
+		_appState = RUNNING;
+		break;
+	case Game::GAME_OVER:
+		init();
+		_appState = RUNNING;
+		break;
+	default:        break;
+	}
 }
 
 // Gere quand la fenetre est redimensionner
 View& Game::handleResizeWindow()
 {
-    Vector2f size = static_cast<Vector2f>(_window.getSize());
+	Vector2f size = static_cast<Vector2f>(_window.getSize());
 
-    // Garatin une taille Minimum de la fenetre
-    size.x = MAX(size.x, MIN_WINDOW_WIDTH);
-    size.y = MAX(size.y, MIN_WINDOW_HEIGHT);
+	// Garatin une taille Minimum de la fenetre
+	size.x = MAX(size.x, MIN_WINDOW_WIDTH);
+	size.y = MAX(size.y, MIN_WINDOW_HEIGHT);
 
-    // Apply possible size changes
-    _window.setSize(static_cast<Vector2u>(size));
+	// Apply possible size changes
+	_window.setSize(static_cast<Vector2u>(size));
 
-    // Reset view
-    _view[NEUTRAL] = View(FloatRect(0.f, 0.f, size.x, size.y));
-    _view[CAMERA] = View(FloatRect(0.f, 0.f, size.x, size.y));
-    _view[FOLLOW] = View(FloatRect(0.f, 0.f, size.x, size.y));
-    _view[FOLLOW_Y] = View(FloatRect(0.f, 0.f, size.x, size.y));
+	// Reset view
+	_view[NEUTRAL] = View(FloatRect(0.f, 0.f, size.x, size.y));
+	_view[CAMERA] = View(FloatRect(0.f, 0.f, size.x, size.y));
+	_view[FOLLOW] = View(FloatRect(0.f, 0.f, size.x, size.y));
+	_view[FOLLOW_Y] = View(FloatRect(0.f, 0.f, size.x, size.y));
 
-    return _view[NEUTRAL];
+	return _view[NEUTRAL];
 }
 
 // Observe l'etat des touches de claviers
 void Game::handleKeypress()
 {
-    if (_appState == RUNNING)
-    {
-        handleArrowKeys();
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Escape))
-        quitApplication();
+	if (_appState == RUNNING)
+	{
+		handleArrowKeys();
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Escape))
+		quitApplication();
 
-    if (Keyboard::isKeyPressed(Keyboard::Num0))
-        initWorldMap();
+	if (Keyboard::isKeyPressed(Keyboard::Num0))
+		initWorldMap();
 
-    // Change weapon
-    if (Keyboard::isKeyPressed(Keyboard::Num1))
-    {
-        _currentTool = BUILD;
-        _player.setNoWeapon(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num2))
-    {
-        _currentTool = UZI;
-        _player.setUzi(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num3))
-    {
-        _currentTool = ASSAULT;
-        _player.setAssault(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num4))
-    {
-        _currentTool = SNIPER;
-        _player.setSniper(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num5))
-    {
-        _currentTool = SHOT_GUN;
-        _player.setShotgun(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num6))
-    {
-        _currentTool = BULLET_HELL;
-        _player.setBulletHell(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num7))
-    {
-        _currentTool = SLOW_MO;
-        _player.setNoWeapon(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num8))
-    {
-        _currentTool = HOMING;
-        _player.setHoming(_frameRun);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Num9))
-    {
-        _currentTool = SPHERE_SHIELD;
-        _player.setNoWeapon(_frameRun);
-    }
+	// Change weapon
+	if (Keyboard::isKeyPressed(Keyboard::Num1))
+	{
+		_currentTool = BUILD;
+		_player.setNoWeapon(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num2))
+	{
+		_currentTool = UZI;
+		_player.setUzi(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num3))
+	{
+		_currentTool = ASSAULT;
+		_player.setAssault(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num4))
+	{
+		_currentTool = SNIPER;
+		_player.setSniper(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num5))
+	{
+		_currentTool = SHOT_GUN;
+		_player.setShotgun(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num6))
+	{
+		_currentTool = BULLET_HELL;
+		_player.setBulletHell(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num7))
+	{
+		_currentTool = SLOW_MO;
+		_player.setNoWeapon(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num8))
+	{
+		_currentTool = HOMING;
+		_player.setHoming(_frameRun);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Num9))
+	{
+		_currentTool = SPHERE_SHIELD;
+		_player.setNoWeapon(_frameRun);
+	}
 }
 
 void Game::handleArrowKeys()
 {
-    _player.isMoving(false);
+	_player.isMoving(false);
 
-    // Keyboard Arrow
-    if (Keyboard::isKeyPressed(Keyboard::Up) ||
-        Keyboard::isKeyPressed(Keyboard::W) ||
-        Keyboard::isKeyPressed(Keyboard::Space))
-    {
-        _view[CAMERA].move(0, -ARROW_EXPLORE);
+	// Keyboard Arrow
+	if (Keyboard::isKeyPressed(Keyboard::Up) ||
+		Keyboard::isKeyPressed(Keyboard::W) ||
+		Keyboard::isKeyPressed(Keyboard::Space))
+	{
+		_view[CAMERA].move(0, -ARROW_EXPLORE);
 
-        if (playerIsOnTheGround())
-            _player.startJump();
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Right) ||
-        Keyboard::isKeyPressed(Keyboard::D))
-    {
-        _player.isMoving(true);
-        _player.lookToTheRight();
-        _view[CAMERA].move(ARROW_EXPLORE, 0);
-        tryToMove(RIGHT, _player);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Down) ||
-        Keyboard::isKeyPressed(Keyboard::S))
-    {
-        _view[CAMERA].move(0, ARROW_EXPLORE);
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Left) ||
-        Keyboard::isKeyPressed(Keyboard::A))
-    {
-        _player.isMoving(true);
-        _player.lookToTheLeft();
-        _view[CAMERA].move(-ARROW_EXPLORE, 0);
-        tryToMove(LEFT, _player);
-    }
+		if (playerIsOnTheGround())
+			_player.startJump();
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Right) ||
+		Keyboard::isKeyPressed(Keyboard::D))
+	{
+		_player.isMoving(true);
+		_player.lookToTheRight();
+		_view[CAMERA].move(ARROW_EXPLORE, 0);
+		tryToMove(RIGHT, _player);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Down) ||
+		Keyboard::isKeyPressed(Keyboard::S))
+	{
+		_view[CAMERA].move(0, ARROW_EXPLORE);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Left) ||
+		Keyboard::isKeyPressed(Keyboard::A))
+	{
+		_player.isMoving(true);
+		_player.lookToTheLeft();
+		_view[CAMERA].move(-ARROW_EXPLORE, 0);
+		tryToMove(LEFT, _player);
+	}
 }
 
 // Gere quand la roulette de souris a changer
 void Game::handleMouseWheelMoved()
 {
-    if (_currentView != NULL_VIEW)
-        _view[_currentView].zoom(1 - (_event.mouseWheel.delta * 0.125f));
+	if (_currentView != NULL_VIEW)
+		_view[_currentView].zoom(1 - (_event.mouseWheel.delta * 0.125f));
 }
 
 // Gere quand un bouton de souris est clicker
 void Game::handleMouseButtonPressed()
 {
-    if (isMouseInWindow()
-        && !areOnTheSameSquare(_mouseMagnet, _player))
-    {
-        int c = _mouseCoord.getPosition().x / TILE_SIZE;
-        int l = _mouseCoord.getPosition().y / TILE_SIZE;
+	if (isMouseInWindow()
+		&& !areOnTheSameSquare(_mouseMagnet, _player))
+	{
+		int c = _mouseCoord.getPosition().x / TILE_SIZE;
+		int l = _mouseCoord.getPosition().y / TILE_SIZE;
 
-        if (Mouse::isButtonPressed(Mouse::Left))
-        {
-            //Placer un bloc
-            if (isMouseInMap() && _currentTool == BUILD)
-                insertBlockAtMouse(c, l);
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			//Placer un bloc
+			if (isMouseInMap() && _currentTool == BUILD)
+				insertBlockAtMouse(c, l);
 
-            //Tirer le nombre de bullets
-            if (toolIsAShooter() && _player.delayIsReady(_frameRun))
-            {
-                Character* target = nullptr;
+			//Si on est prêt à tirer
+			if (toolIsAShooter() && _player.delayIsReady(_frameRun))
+			{
+				Character* target = nullptr;
 
-                if (_currentTool == HOMING)
-                {
-                    for (Crawler& c : _bats)
-                    {
-                        //Détermine la target
-                        if (areOnTheSameSquare(_mouseMagnet, c))
-                        {
-                            target = &c;
-                            break;
-                        }
-                    }
-                }
+				if (_currentTool == HOMING)
+				{
+					//On vise la _bat la plus proche du joueur
+					if (_bats.size() > 0)
+					{
+						target = &*(_bats.begin());
+						float closestEnemyDist = distanceBetweenMP(_player, *(_bats.begin()));
 
-                for (int i = 0; i < _player.getWeaponNbBulletsFired(); i++)
-                {
-                    shootBullet(target);
-                }
-            }
-        }
-        if (Mouse::isButtonPressed(Mouse::Right))
-        {
-            //Enlever un bloc
-            if (isMouseInMap() && _currentTool == BUILD)
-                removeBlockAtMouse(c, l);
-        }
-    }
+						for (Crawler& c : _bats)
+						{
+							float actualEnemyDist;
+
+							if ((actualEnemyDist = distanceBetweenMP(_player, c)) < closestEnemyDist)
+							{
+								closestEnemyDist = actualEnemyDist;
+								target = &c;
+							}
+						}
+					}
+
+					//Si aucune _bat existe, vise le boss
+					else
+						target = &_boss;
+				}
+
+				//Tirer le nombre de bullets
+				for (int i = 0; i < _player.getWeaponNbBulletsFired(); i++)
+					shootBullet(target);
+			}
+		}
+		if (Mouse::isButtonPressed(Mouse::Right))
+		{
+			//Enlever un bloc
+			if (isMouseInMap() && _currentTool == BUILD)
+				removeBlockAtMouse(c, l);
+		}
+	}
 }
 
 void Game::shootBullet(Character *target)
 {
-    _bullets.push_back(Bullet());
+	_bullets.push_back(Bullet());
 
-    if (MUSIQUE)
-        _bullets.back().play(_buffBullet);
+	if (MUSIQUE)
+		_bullets.back().play(_buffBullet);
 
-    _bullets.back().setPositionExact(
-        _player.getExactX(),
-        _player.getExactY() - HALF_TILE_SIZE);
+	_bullets.back().setPositionExact(
+		_player.getExactX(),
+		_player.getExactY() - HALF_TILE_SIZE);
 
-    if (target == nullptr)
-    {
-        _bullets.back().aim(
-            _mouseCoord.getPosition().x,
-            _mouseCoord.getPosition().y,
-            _player.getWeaponAccuracy());
-    }
-    else
-    {
-        _bullets.back().setTarget(target);
-        _bullets.back().aim(*target);
-    }
+	if (target == nullptr)
+	{
+		_bullets.back().aim(
+			_mouseCoord.getPosition().x,
+			_mouseCoord.getPosition().y,
+			_player.getWeaponAccuracy());
+	}
+	else
+	{
+		_bullets.back().setTarget(target);
+		_bullets.back().aim(*target);
+	}
 
-    _bullets.back().setSpeed(_player.getWeaponBulletSpeed());
-    _bullets.back().setDamage(_player.getWeaponDamage());
-    _player.delayReset(_frameRun);
+	_bullets.back().setSpeed(_player.getWeaponBulletSpeed());
+	_bullets.back().setDamage(_player.getWeaponDamage());
+	_player.delayReset(_frameRun);
 }
